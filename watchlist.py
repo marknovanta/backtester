c0556dcdd4d93d702fa900297b9351ca5f448a12
Marco T. <107734315+marknovanta@users.noreply.github.com>
2024-05-21 18:30:03 +0200

    Filter swing watchlist excluding all negative CAGRs

--- watchlist.py
+++ watchlist.py
@@ -13,7 +13,7 @@ tickers_invest = [
 ]
 
 # SWNIG
-tickers_swing = [
+""" tickers_swing = [
     'ACI','AES','AGR','ALLY','AMH','APA','ARCC','BAC','BAX','BEN','BEPC','BKR',
     'BMY','CAG','CFG','CG','CMCSA','CNA','CNP','CPB','CQP','CRBG','CSCO','CSX',
     'CTRA','DOC','EPD','EQH','EQT','ET','EXC','F','FE','FITB','FLEX','FOX','FTI',
@@ -29,5 +29,25 @@ tickers_swing = [
     'L','LNT','LW','MAS','MCHP','MDLZ','MET','MKC','MRVL','NDAQ','NEE','NKE','NRG','NTRS','O',
     'OKE','OMC','OTIS','OVV','OXY','PEG','PFG','PM','RBA','REG','SBUX','SCHW','SCI','SO','SRE',
     'SSNC','STT','STX','SWK','SWKS','SYY','TAP','TECH','TJX','TRU','TSN','TXT','VRT','VST',
+    'WEC','WFC','WMT','WPC','WRB','WRK','WYNN','XEL'
+] """
+
+# SWNIG FILTERED (removed tickers with negative CAGR)
+tickers_swing = [
+    'ACI','AES','AMH','BAC','BAX','BEN','BKR',
+    'BMY','CAG','CMCSA','CNA','CNP','CPB','CQP','CRBG','CSCO','CSX',
+    'CTRA','DOC','EPD','EQT','ET','EXC','F','FE','FITB','FLEX','FTI',
+    'GEN','GLW','HAL','HBAN','HPQ','HRL','INTC','INVH',
+    'IPG','JEF','JNPR','KEY','KIM','KVUE','LKQ','LUV','LVS','MO','MPLX',
+    'MRO','NI','NWS','OVV','PAA','PFE','PPL','RF','SIRI',
+    'SLB','T','TFC','UDR','USB','VTR','VTRS','WBA','WES','WMB',
+    'WTRG',
+    'ADM','AEP','AFL','AIG','AOS','BALL','BBWI','BBY','BK','BRKR','BRO',
+    'C','CAH','CARR','CCK','CF','CHK','CMS','COO','CTSH','CTVA','CVS','D','DAL','DD',
+    'DINO','DVN','EBAY','ED','EIX','ELS','EMN','EQR','EVRG','EWBC','FAST','FCX',
+    'FIS','FTV','GEHC','GGG','GILD','GIS','IFF','IR','K','KO','KR',
+    'L','LNT','LW','MAS','MCHP','MET','MKC','MRVL','NDAQ','NEE','NKE','NRG','NTRS','O',
+    'OKE','OMC','OTIS','OVV','OXY','PEG','PFG','RBA','REG','SBUX','SCHW','SCI','SO','SRE',
+    'SSNC','STT','STX','SWK','SWKS','SYY','TECH','TJX','TSN','TXT','VRT',
     'WEC','WFC','WMT','WPC','WRB','WRK','WYNN','XEL'
 ]
