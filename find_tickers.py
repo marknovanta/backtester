ab8fec61541a488edcf713e9d67a4e070ce037f2
Marco T. <107734315+marknovanta@users.noreply.github.com>
2024-05-23 06:32:17 +0200

    Add code to write on a txt file the stocks it finds

--- find_tickers.py
+++ find_tickers.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from dotenv import load_dotenv
+import json
 
 def get_tickers():
     load_dotenv()
@@ -12,12 +13,20 @@ def get_tickers():
         data = r.json()
         exchs = ['NASDAQ', 'NYSE']
         tickers = []
+        filtered_data = []
         for i in data:
             try:
                 if (float(i['price']) < 100) and (float(i['price']) >= 10) and (i['type'] == 'stock') and (i['exchangeShortName'] in exchs) and ('.' not in i['symbol']) and (' ' not in i['symbol']) and ('-' not in i['symbol']):
                     tickers.append(i['symbol'])
+                    filtered_data.append(i)
             except:
                 continue
+
+        with open('found_tickers.txt', 'w') as file:
+            for i in filtered_data:
+                file.write(json.dumps(i))
+                file.write('\n')
+
         return tickers
     except requests.exceptions.RequestException as e:
         print('Error fetching data:', e)
